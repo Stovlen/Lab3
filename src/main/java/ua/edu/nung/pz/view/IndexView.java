acfc95919f1fbec012f081692d64efded6fe2634
Olexandr Stovban <stovbanolexandr@gmail.com>
2024-03-22 13:18:06 +0200

    2

--- src/main/java/ua/edu/nung/pz/view/IndexView.java
+++ src/main/java/ua/edu/nung/pz/view/IndexView.java
@@ -1,23 +1,27 @@
 package ua.edu.nung.pz.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class IndexView {
-    public static String getPage(String title, String body) {
-        return "<!doctype html>\n" +
-                "<html lang=\"en\">\n" +
-                "  <head>\n" +
-                "    <meta charset=\"utf-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
-                "    <title>" + title + "</title>\n" +
-                "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">\n" +
-                "  </head>\n" +
-                "  <body>\n" +
-                body +
-                "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\" crossorigin=\"anonymous\"></script>\n" +
-                "  </body>\n" +
-                "</html>";
+    private String path;
+    private static IndexView indexView = new IndexView();
+    private IndexView() {}
+
+    public static IndexView getInstance() {
+        return indexView;
+    }
+    public String getPage(String title, String body) {
+        return getHtml("emptyPage")
+                .replace("<!--####title###-->", title)
+                .replace("<!--####body###-->", body);
     }
 
-    public static String getBody(String header, String footer, String context) {
+    public String getBody(String header, String footer, String context) {
         return header +
                 "<div class=\"container\">" +
                 context +
@@ -25,58 +29,61 @@ public class IndexView {
                 footer;
     }
 
-    public static String getHeader(String header) {
-        return "<nav class=\"navbar navbar-expand-lg bg-body-tertiary\">\n" +
-                "  <div class=\"container-fluid\">\n" +
-                "    <a class=\"navbar-brand\" href=\"/\">GreenShop</a>\n" +
-                "    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-                "      <span class=\"navbar-toggler-icon\"></span>\n" +
-                "    </button>\n" +
-                "    <div class=\"collapse navbar-collapse\" id=\"navbarNav\">\n" +
-                "      <ul class=\"navbar-nav\">\n" +
-                "        <li class=\"nav-item\">\n" +
-                "          <a class=\"nav-link active\" aria-current=\"page\" href=\"/\">Home</a>\n" +
-                "        </li>\n" +
-                "        <li class=\"nav-item\">\n" +
-                "          <a class=\"nav-link\" href=\"/pricing\">Pricing</a>\n" +
-                "        </li>\n" +
-                "\t<li class=\"nav-item\">\n" +
-                "          <a class=\"nav-link\" href=\"/contacts\">Contacts</a>\n" +
-                "        </li>\n" +
-                "      </ul>\n" +
-                "    </div>\n" +
-                "  </div>\n" +
-                "</nav>";
+    public String getHeader(String userName) {
+        String html = getHtml("headerPartial");
+        if (userName.length() > 0) {
+            html = conditionalTextDelete(html, "usernameNotLogin")
+                    .replace("<!--###username###-->", userName);;
+        } else {
+            html = conditionalTextDelete(html, "usernameLoginedIn");
+        }
+        return html;
     }
 
-    public static String getFooter(String footer) {
-        return "<footer class=\"container-fluid mt-5\">\n" +
-                "<div class=\"bg-dark text-secondary px-4 py-5 text-center\">\n" +
-                "    <div class=\"py-5\">\n" +
-                "      <h1 class=\"display-5 fw-bold text-white\"> <span class=\"text-success\">Green Shop</span>  </h1>\n" +
-                "      <div class=\"col-lg-6 mx-auto\">\n" +
-                "        <p class=\"fs-5 mb-4\">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>\n" +
-                "        <div class=\"d-grid gap-2 d-sm-flex justify-content-sm-center\">\n" +
-                "          <button type=\"button\" class=\"btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold\">Custom button</button>\n" +
-                "          <button type=\"button\" class=\"btn btn-outline-light btn-lg px-4\">Secondary</button>\n" +
-                "        </div>\n" +
-                "      </div>\n" +
-                "    </div>\n" +
-                "  </div>\n" +
-                "</footer>";
+    public String getFooter(String footer) {
+        return getHtml("footerPartial");
     }
 
-    public static String getLoginForm() {
-        return  "<form action=\"login\" class=\"my-5\">\n" +
-                "  <div class=\"mb-3\">\n" +
-                "    <label for=\"webshop-login-form-username\" class=\"form-label\">Email address</label>\n" +
-                "    <input type=\"email\" class=\"form-control\" id=\"webshop-login-form-username\" name=\"username\">\n" +
-                "  </div>\n" +
-                "  <div class=\"mb-3\">\n" +
-                "    <label for=\"webshop-login-form-password\" class=\"form-label\">Password</label>\n" +
-                "    <input type=\"password\" class=\"form-control\" id=\"webshop-login-form-password\" name=\"password\">\n" +
-                "  </div>\n" +
-                "  <button type=\"submit\" class=\"btn btn-primary\">Submit</button>\n" +
-                "</form>";
+    public String getLoginForm() {
+        return getHtml("loginFormPartial");
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    private String getHtml(String filename) {
+        StringBuilder strb = new StringBuilder("\n");
+        Path file = Paths.get(path + filename + ".html");
+        Charset charset = Charset.forName("UTF-8");
+
+        try (BufferedReader reader = Files.newBufferedReader(file, charset))
+        {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                strb.append(line).append("\n");
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return strb.toString();
+    }
+
+    private String conditionalTextDelete(String html, String markToDelete) {
+        String startMarker = "<!--Variable ###" + markToDelete + "###-->";
+        String endMarker = "<!--endVariable-->";
+        int startIndex = html.indexOf(startMarker);
+        if (startIndex == -1) {
+            return html;
+        }
+        int endIndex = html.indexOf(endMarker, startIndex);
+        if (endIndex == -1) {
+            return html;
+        }
+        String firstPart = html.substring(0, startIndex);
+        String endPart = html.substring(endIndex + endMarker.length());
+        return firstPart + endPart;
     }
 }
